fd7d165e2412950dd93d86dab0d9dbd09ce61af0
Reedy <reedy@wikimedia.org>
2016-07-10 22:07:13 +0100

    Convert Capiunto to extension registration
    Change-Id: I6118fc67168d8219c81f7c46e26677adedf238e6

--- Capiunto.php
+++ Capiunto.php
@@ -7,36 +7,15 @@
  * @author Marius Hoch < hoo@online.de >
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( "Not an entry point.\n" );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Capiunto' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Capiunto'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for Capiunto extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the Capiunto extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'Capiunto',
-	'author' => 'Marius Hoch',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Capiunto',
-	'descriptionmsg' => 'capiunto-desc',
-	'license-name' => 'GPL-2.0+',
-);
-
-$wgMessagesDirs['Capiunto'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['Capiunto'] = __DIR__ . '/Capiunto.i18n.php';
-
-$wgAutoloadClasses['Capiunto\Hooks']		= __DIR__ . '/includes/Hooks.php';
-$wgAutoloadClasses['Capiunto\LuaLibrary']					= __DIR__ . '/includes/LuaLibrary.php';
-$wgAutoloadClasses['Capiunto\Test\InfoboxModuleTest']		= __DIR__ . '/tests/phpunit/includes/lua/InfoboxTest.php';
-
-$wgHooks['UnitTestsList'][] 				= '\Capiunto\Hooks::registerUnitTests';
-$wgHooks['ScribuntoExternalLibraries'][] 	= '\Capiunto\Hooks::registerScribuntoLibraries';
-$wgHooks['ScribuntoExternalLibraryPaths'][] 	= '\Capiunto\Hooks::registerScribuntoExternalLibraryPaths';
-
-$commonModuleInfo = array(
-	'localBasePath' => __DIR__ . '/resources',
-	'remoteExtPath' => 'Capiunto/resources',
-);
-
-$wgResourceModules['capiunto.infobox.main'] = array(
-	'styles' => 'infobox.css',
-	'position' => 'top'
-) + $commonModuleInfo;
