566f159266b3abd1d833bc7cc0d735c1e50496c2
Umherirrender <umherirrender_de.wp@web.de>
2023-11-01 14:30:04 +0100

    Use HookHandlers for Scribunto hooks
    Remove optional return value from hook handlers
    
    Bug: T346512
    Change-Id: Icc17a524828d81ce138732e4afaf1b4417d12fe8

--- includes/ScribuntoHooks.php
+++ includes/ScribuntoHooks.php
@@ -2,33 +2,37 @@
 
 namespace Capiunto;
 
+use MediaWiki\Extension\Scribunto\Hooks\ScribuntoExternalLibrariesHook;
+use MediaWiki\Extension\Scribunto\Hooks\ScribuntoExternalLibraryPathsHook;
+
 /**
  * File defining the hook handlers for the Capiunto extension.
+ * All hooks from the Scribunto extension which is optional to use with this extension.
  *
  * @license GPL-2.0-or-later
  *
  * @author Marius Hoch < hoo@online.de >
  */
-class Hooks {
+class ScribuntoHooks implements
+	ScribuntoExternalLibrariesHook,
+	ScribuntoExternalLibraryPathsHook
+{
 
 	/**
 	 * External Lua library for Scribunto
 	 *
 	 * @param string $engine
 	 * @param array &$extraLibraries
-	 * @return bool
 	 */
-	public static function registerScribuntoLibraries( $engine, array &$extraLibraries ) {
+	public function onScribuntoExternalLibraries( string $engine, array &$extraLibraries ): void {
 		if ( $engine !== 'lua' ) {
-			return true;
+			return;
 		}
 
 		$extraLibraries['capiunto'] = [
 			'class' => LuaLibrary::class,
 			'deferLoad' => true
 		];
-
-		return true;
 	}
 
 	/**
@@ -36,20 +40,17 @@ class Hooks {
 	 *
 	 * @param string $engine
 	 * @param array &$extraLibraryPaths
-	 * @return bool
 	 */
-	public static function registerScribuntoExternalLibraryPaths(
-		$engine,
+	public function onScribuntoExternalLibraryPaths(
+		string $engine,
 		array &$extraLibraryPaths
-	) {
+	): void {
 		if ( $engine !== 'lua' ) {
-			return true;
+			return;
 		}
 
 		// Path containing pure Lua libraries that don't need to interact with PHP
 		$extraLibraryPaths[] = __DIR__ . '/lua/pure';
-
-		return true;
 	}
 
 }
